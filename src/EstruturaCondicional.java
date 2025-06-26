2ad631ea82b73345a3841ecb0ab8b193218c2354
kayna <kaynanoliveira357@gmail.com>
2025-06-26 02:07:55 -0300

    Projeto java

---
+++ src/EstruturaCondicional.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class EstruturaCondicional {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite sua idade: ");
+        int idade = scanner.nextInt();
+
+        if(idade >= 18){
+            System.out.println("Maior de idade");
+        }else {
+            System.out.println("Menor de idade");
+        }
+    }
+}
