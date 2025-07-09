547b5022e299a5242c8d473e1e7cac8c7a0138f2
kayna <kaynanoliveira357@gmail.com>
2025-07-09 20:57:58 -0300

    Exercicio

--- src/exercicio.java
+++ src/exercicio.java
@@ -2,16 +2,39 @@ import java.util.Scanner;
 
 public class exercicio {
     public static void main(String[] args) {
-        Scanner leitorScanner = new Scanner(System.in);
-        double valorProduto;
-        double prestacao;
+        Scanner leitor = new Scanner(System.in);
 
-        System.out.print("Digite o valor do produto: ");
-        valorProduto = leitorScanner.nextDouble();
+        int anoVeiculo;
+        float valorDesconto;
+        float valorVeiculo;
+        float valorComDesconto;
+        int totalSemiNovos = 0;
+        int totalDeCarros = 0;
+        char desejaContinuar = 's';
 
-        prestacao = valorProduto / 5;
+        while(desejaContinuar == 'S' || desejaContinuar == 's'){
+            System.out.println("Digite o ano do veiculo: ");
+            anoVeiculo = leitor.nextInt();
 
-        System.out.println("Valor do produto = " + valorProduto);
-        System.out.println("Prestações = " + prestacao);
+            System.out.println("Digite o valor do veiculo: ");
+            valorVeiculo = leitor.nextFloat();
+
+            if(anoVeiculo <= 2000){
+                valorDesconto = valorVeiculo * 0.12f;
+                valorComDesconto = valorVeiculo - valorDesconto;
+                totalSemiNovos++;
+            }else{
+                valorDesconto = valorVeiculo * 0.07f;
+                valorComDesconto = valorVeiculo - valorDesconto;
+            }
+            System.out.println("O valor do desconto foi de: " + valorDesconto);
+            System.out.println("Valor com desconto: " + valorComDesconto);
+            totalDeCarros++;
+
+            System.out.println("Deseja continuar? S - Sim / N - Não: ");
+            desejaContinuar = leitor.next().charAt(0);
+        }
+        System.out.println("Total de carros semi novos = " + totalSemiNovos);
+        System.out.println("Total de carros = " + totalDeCarros);
     }
 }
